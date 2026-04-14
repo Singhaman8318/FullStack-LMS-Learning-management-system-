0211536615b283d686346b73766e4cdd35ab4d3a
Aman Singh <p.amansinghji@gmail.com>
2026-04-15 01:11:37 +0530

    final changes done

--- Server/app.js
+++ Server/app.js
@@ -85,8 +85,9 @@ const app = express();
 
 // --- 1. Global Middlewares ---
 app.use(cors({
-  origin: process.env.FRONTEND_URL || '*', 
-  credentials: true
+  origin: ["https://fullstack-lms-learning-management-system-2syi.onrender.com", "http://localhost:5173"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }));
 app.use(express.json());
 app.use(cookieParser());
